42bfa3c9c1817ae8777d979a22a23ea42e7b2852
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-20 20:28:13 +0530

    ApproachOne Exection no result

--- src/test/java/jQueryUI/HandlingSlider.java
+++ src/test/java/jQueryUI/HandlingSlider.java
@@ -2,8 +2,11 @@ package jQueryUI;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -22,8 +25,16 @@ public class HandlingSlider {
 	}
 
 	@Test
-	public void handlingSlider() {
-
+	public void handlingSlider() throws Exception{
+		// Action Class
+		Actions ac = new Actions(driver);
+		WebElement demoFrame = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
+		driver.switchTo().frame(demoFrame);
+		WebElement sliderPionter = driver.findElement(By.xpath("//span[@class='ui-slider-handle ui-corner-all ui-state-default']"));
+		System.out.println("Current slider location : "+sliderPionter.getLocation());
+		ac.dragAndDropBy(sliderPionter, 500, 3);
+		Thread.sleep(3000);
+		System.out.println("Location after moving : "+sliderPionter.getLocation());
 	}
 
 	@AfterClass
